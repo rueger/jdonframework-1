ff4c5be65f1da62bd461ad11cba48b2f7adf7721
nk3310 <nk3310@126.com>
2015-11-12 19:19:52 +0800

    feature-02:用junit测试testSelectUserByID()
    相关改动如下：
    mybatis-test-config.xml
    +  <typeAliases>
    +     <typeAlias type="com.jdon.framework.test.domain.UserModel" alias="User"/>
    +  </typeAliases>
    
    TestMapper.xml
    +  <resultMap id="userResultMap" type="User">
    +     <id property="userId" column="USERID" />
    +     <result property="username" column="NAME"/>
    +  </resultMap>
    
    +  <select id="selectuserbyid" resultMap="userResultMap">
    +     select USERID,NAME
    +     from testuser
    +     where userId = #{id}
       </select>

--- example/jdonMVC+CQRS+ES/src/test/java/com/jdonframework/test/mybatis/MybatisBuilderTest.java
+++ example/jdonMVC+CQRS+ES/src/test/java/com/jdonframework/test/mybatis/MybatisBuilderTest.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import com.jdon.controller.AppUtil;
+import com.jdon.framework.test.domain.UserModel;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -18,7 +19,9 @@ public class MybatisBuilderTest extends TestCase {
 		
 		mybatisBuilderTest.testSqlSessionFactoryBuilder();
 		
-		mybatisBuilderTest.testSelectUserID();
+		mybatisBuilderTest.testSelectUserIdById();
+		
+		mybatisBuilderTest.testSelectUserByID();
 
 	}
 
@@ -33,7 +36,7 @@ public class MybatisBuilderTest extends TestCase {
 		appUtil.clear();
 	}
 	
-	public void testSelectUserID() {
+	public void testSelectUserIdById() {
 		
 		AppUtil appUtil = new AppUtil();
 		
@@ -46,7 +49,7 @@ public class MybatisBuilderTest extends TestCase {
 		String userid;
         
         try {
-        	  userid = (String) session.selectOne("com.jdon.framework.test.user.selectuseridbyid", 1);
+        	  userid = (String) session.selectOne("com.jdonframework.test.user.selectuseridbyid", 1);
         } finally {
           session.close();
         }
@@ -56,6 +59,35 @@ public class MybatisBuilderTest extends TestCase {
         System.out.println("userid = " + userid);
         
         System.out.println("testSelectUserID() ok ");
+		
+		appUtil.clear();
+	}
+	
+	
+	
+	public void testSelectUserByID() {
+		
+		AppUtil appUtil = new AppUtil();
+
+		TestMybatisBuilder mybatisBuilder = (TestMybatisBuilder) appUtil.getComponentInstance("TestSqlSessionFactory");
+		
+        SqlSessionFactory sqlSessionFactory = mybatisBuilder.getSqlSessionFactory();
+		
+		SqlSession session = sqlSessionFactory.openSession();
+		
+		UserModel user = null;
+		
+		try {
+		  user = session.selectOne("com.jdonframework.test.user.selectuserbyid", 1);
+        } finally {
+        session.close();
+        }
+		
+		System.out.println("user.Userid = " + user.getUserId());
+		
+		System.out.println("user.username = " + user.getUsername());
+		
+		System.out.println("testSelectUserByID() ok ");
 		
 		appUtil.clear();
 	}
